f3d1b59202194b7d750dc1539e5ce03f11440d71
UsUsStudios <biruni056@gmail.com>
2024-09-21 07:54:08 -0600

    Set up main code structure

---
+++ src/Computer.java
@@ -0,0 +1,5 @@
+public class Computer {
+    public static void run() {
+        System.out.println("I've been summoned!");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Summoning in progress...");
+        Computer.run();
+        System.out.println("Summoning complete!");
+    }
+}
